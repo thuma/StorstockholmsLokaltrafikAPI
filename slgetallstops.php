3d86673552bfbacd3887f2e13b343f5070b1cb9d
Martin Thuresson <martin.thure@gmail.com>
2013-11-20 07:10:43 +0100

    Added just test version.

--- slgetallstops.php
+++ slgetallstops.php
@@ -1,34 +1,39 @@
 <?php
 
+$cordkey = "";
+
 $ch = curl_init();
 curl_setopt_array($ch, array(
     CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_SSL_VERIFYHOST => false,
     CURLOPT_RETURNTRANSFER => true,
 ));
-$idlistfile = 'allsiteids.json';
 
-if(is_file($idlistfile)==FALSE){
-	$slidlist = array();
-	for($i = 1; $i < 10; $i++){
-		curl_setopt($ch ,CURLOPT_URL, 'https://api.trafiklab.se/sl/realtid/GetSite.json?stationSearch='.$i.'&key=');
-		$data = curl_exec($ch);
-		$jsonobject = json_decode($data);
-		print_r($jsonobject);
-		$idarray = $jsonobject->Hafas->Sites->Site;
-		foreach ($idarray as $row){
-			$slidlist[] = $row->Number;
-		}
-	}
-file_put_contents($idlistfile,json_encode($slidlist));
-}
-$slidlist = json_decode(file_get_contents($idlistfile));
-
-$max = count($slidlist) - 1;
-for($i = 0; i < $max; $i++){
-	curl_setopt($ch ,CURLOPT_URL, 'https://api.trafiklab.se/sl/reseplanerare.json?S='.$slidlist[$i].'&Z='.$slidlist[$max].'&Date=10.12.2013&Time=12%3A00&Timesel=depart&Lang=sv&key=');
-	print 'https://api.trafiklab.se/sl/reseplanerare.json?S='.$slidlist[$i].'&Z='.$slidlist[$max].'&Date=10.12.2013&Time=12%3A00&Timesel=depart&Lang=sv&key=';
-	$max = $max - 1;
+$dataarray = array();
+for($i = 1001; $i < 1000; $i++){
+	curl_setopt($ch ,CURLOPT_URL, 'https://api.trafiklab.se/sl/reseplanerare.json?S=1000&Z='.$i.'&Date=10.12.2013&Time=12%3A00&Timesel=depart&Lang=sv&key='.$cordkey);
+	$data = curl_exec($ch);
+	$jsonobject = json_decode($data);
+	
+	$fran = new stdClass;
+	$fran->id = $slidlist[$i];
+	$fran->name = $jsonobject->HafasResponse->Trip[0]->Summary->Origin->{'#text'};
+	$fran->lat = $jsonobject->HafasResponse->Trip[0]->Summary->Origin->{'@y'};
+	$fran->lon = $jsonobject->HafasResponse->Trip[0]->Summary->Origin->{'@x'};
+	$fran->sa = $jsonobject->HafasResponse->Trip[0]->Summary->Origin->{'@sa'};
+	
+	$till = new stdClass;
+	$till->id = $slidlist[$max];
+	$till->name = $jsonobject->HafasResponse->Trip[0]->Summary->Destination->{'#text'};
+	$till->lat = $jsonobject->HafasResponse->Trip[0]->Summary->Destination->{'@y'};
+	$till->lon = $jsonobject->HafasResponse->Trip[0]->Summary->Destination->{'@x'};
+	$till->sa = $jsonobject->HafasResponse->Trip[0]->Summary->Destination->{'@sa'};
+	$till->id = strval($i);
+	
+	$dataarray[1000]=$fran;
+	$dataarray[$i]=$till;
+	file_put_contents('coord.json',json_encode($dataarray));
 	}
 curl_close($ch);
+
 ?>
