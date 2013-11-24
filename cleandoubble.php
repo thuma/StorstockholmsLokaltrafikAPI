553babe3384960a042404303c811562ff23c27ea
Martin Thuresson <martin.thure@gmail.com>
2013-11-24 21:05:11 +0100

    Corrected error stil a lott of errors but most are ok.

--- cleandoubble.php
+++ cleandoubble.php
@@ -5,14 +5,12 @@ $temparray = array();
 $data = json_decode(file_get_contents('coord.json'));
 foreach($data as $row){
 $dataarray[$row->sa] = $row;
-$dataarray[$row->sa]->id = substr($row->sa,-4);
 }
 
 foreach($dataarray as $row){
 $temparray[] = $row;
 }
 
-	
 file_put_contents('coord-clean.json',json_encode($temparray));
 
 ?>
